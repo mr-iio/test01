089e3a4121b4b89b5a5b047eec26a3716d5e6c31
iio <iio@merxevo.co.jp>
2023-11-20 18:11:49 +0900

    内部レビュー#01 web_page修正 StatelessWidgetに

--- lib/features/web_page/presentation/screens/web_page.dart
+++ lib/features/web_page/presentation/screens/web_page.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:test01/features/web_page/presentation/widgets/body.dart';
 import 'package:test01/features/web_page/presentation/widgets/bottom_appbar.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test01/shared/models/bookmark_models.dart';
 
-class WebPage extends ConsumerWidget {
+class WebPage extends StatelessWidget {
   final Bookmark bookmarkFormController;
   const WebPage({Key? key, required this.bookmarkFormController})
       : super(key: key);
 
   @override
-  Widget build(BuildContext context, ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(bookmarkFormController.title),
