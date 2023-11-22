014e57d48953373845eb1221c6d44c17b69feb04
iio <iio@merxevo.co.jp>
2023-11-22 09:12:08 +0900

    ブックマーク登録は20件までとする機能を追加

--- lib/features/home_page/presentation/widgets/floating_action_button.dart
+++ lib/features/home_page/presentation/widgets/floating_action_button.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test01/features/home_page/presentation/providers/home_page_provider.dart';
 import 'package:test01/features/home_page/presentation/widgets/add_form/add_form.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class HomeFloatingActionButton extends StatelessWidget {
+class HomeFloatingActionButton extends ConsumerWidget {
   const HomeFloatingActionButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final state = ref.watch(bookmarkNotifierProvider);
     return FloatingActionButton(
       onPressed: () async {
-        await showDialog<void>(
-            context: context, builder: (context) => const AddForm());
+        if (state.data.length < 20) {
+          await showDialog<void>(
+              context: context, builder: (context) => const AddForm());
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(L10n.of(context).maxBookmark),
+            ),
+          );
+        }
       },
       child: const Icon(Icons.add),
     );
